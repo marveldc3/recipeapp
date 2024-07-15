6d1773a3d4f1ab634720bf8de2ba5f422026066e
marveldc3 <162106935+marveldc3@users.noreply.github.com>
2024-07-15 21:07:27 +0200

    0

--- src/components/FavoriteRecipes.tsx
+++ src/components/FavoriteRecipes.tsx
@@ -1,28 +1,28 @@
 // src/components/FavoriteRecipes.tsx
- import React, { useContext } from "react";
-  import { RecipeContext } from "../App.tsx";
-  import RecipeDetail from "./RecipeDetail.tsx"; // Ensure this import is present
+import React, { useContext } from "react";
+import { RecipeContext } from "../App.tsx";
+import RecipeDetail from "./RecipeDetail.tsx"; // Ensure this import is present
 
-  const FavoriteRecipes = () => {
-    const { state } = useContext(RecipeContext);
-    const { favorites } = state;
+const FavoriteRecipes = () => {
+  const { state } = useContext(RecipeContext);
+  const { favorites } = state;
 
-    return (
-      <div>
-        {favorites.length > 0 && (
-          <>
-            <h2>Favorites</h2>
-            <ul>
-              {favorites.map((recipeId) => (
-                <li key={recipeId}>
-                  <RecipeDetail recipeId={recipeId} />
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-      </div>
-    );
-  };
+  return (
+    <div>
+      {favorites.length > 0 && (
+        <>
+          <h2>Favorites</h2>
+          <ul>
+            {favorites.map((recipeId) => (
+              <li key={recipeId}>
+                <RecipeDetail recipeId={recipeId} /> {/* Provide the recipeId prop */}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
 
-  export default FavoriteRecipes;
+export default FavoriteRecipes;
