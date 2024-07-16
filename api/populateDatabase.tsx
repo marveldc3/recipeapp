a2e4966b59969fb6f40734cec9b0f4e520888b26
marveldc3 <162106935+marveldc3@users.noreply.github.com>
2024-07-16 18:19:56 +0200

    0

--- api/populateDatabase.tsx
+++ api/populateDatabase.tsx
@@ -1,26 +1,24 @@
-import { KV } from "@vercel/kv";
-import axios from "axios";
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { kv } from '@vercel/kv';
+import fetch from 'node-fetch';
 
-const kv = new KV();
+const APP_ID = '782a8c61';
+const APP_KEY = 'a1942b18ea7db3be941b03c4050b6b85';
+const EDAMAM_API_URL = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-const APP_ID = "782a8c61";
-const APP_KEY = "a1942b18ea7db3be941b03c4050b6b85";
-const KV_REST_API_TOKEN = "AbioAAIncDFhZmRhN2I0MTNhODQ0ZDNiYTUzOTZiYzFlZjJjNmU2NHAxNDcyNzI";
-const KV_REST_API_URL = "https://helping-reptile-47272.upstash.io";
+const KV_URL ='redis://default:AbioAAIncDFhZmRhN2I0MTNhODQ0ZDNiYTUzOTZiYzFlZjJjNmU2NHAxNDcyNzI@helping-reptile-47272.upstash.io:6379';
+const KV_REST_API_URL = 'https://helping-reptile-47272.upstash.io';
+const KV_REST_API_TOKEN = 'AbioAAIncDFhZmRhN2I0MTNhODQ0ZDNiYTUzOTZiYzFlZjJjNmU2NHAxNDcyNzI';
 
-async function populateDatabase() {
-  const response = await axios.get(
-    `https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}&q=chicken`
-  );
+export default async function handler(request: VercelRequest, response: VercelResponse) {
+  if (process.env.VERCEL_ENV === 'production') {
+    const res = await fetch(EDAMAM_API_URL);
+    const data = await res.json();
 
-  const recipes = response.data.hits;
+    for (const recipe of data.hits) {
+      await kv.set(recipe.recipe.uri, JSON.stringify(recipe.recipe));
+    }
 
-  for (const recipe of recipes) {
-    await kv.hset("recipes", recipe.recipe.label, JSON.stringify(recipe.recipe));
+    response.status(200).json({ message: 'Database populated successfully' });
   }
-}
-
-// Call the function only once during deployment
-if (process.env.VERCEL_ENV === "production" && process.env.VERCEL_DEPLOYMENT_ID) {
-  populateDatabase().catch((error) => console.error(error));
 }
