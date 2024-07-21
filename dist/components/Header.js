0818582197b10fb4aca0ed489a95519d88346f67
marveldc3 <162106935+marveldc3@users.noreply.github.com>
2024-07-21 14:04:43 +0200

    0

--- dist/components/Header.js
+++
@@ -1,9 +0,0 @@
-// src/components/Header.tsx
-import React from "react";
-var Header = function (_a) {
-    var setSearchQuery = _a.setSearchQuery;
-    return (React.createElement("header", null,
-        React.createElement("h1", null, "Recipe App"),
-        React.createElement("input", { type: "text", placeholder: "Search recipes...", onChange: function (e) { return setSearchQuery(e.target.value); } })));
-};
-export default Header;
